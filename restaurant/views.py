f347333126323a4ed201a7fa4cabdceebe809221
iamtun <letuan19431791.iuh@gmail.com>
2024-10-25 15:30:25 +0700

    create api view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
