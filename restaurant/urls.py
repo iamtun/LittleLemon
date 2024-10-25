57f9ffaead1b9594d5aacb7a82fff09c213f988a
iamtun <letuan19431791.iuh@gmail.com>
2024-10-25 15:30:33 +0700

    config urls in app

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import index
+from django.urls import path, include
+from .views import MenuItemsView, SingleMenuItemView, BookingViewSet
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+
+router.register(r'tables', BookingViewSet, basename='menu')
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('menu/', MenuItemsView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
+    path('booking/', include(router.urls)),
 ]
