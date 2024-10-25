694180f2d92c828648dbd96c1a0f3ed4468e0419
iamtun <letuan19431791.iuh@gmail.com>
2024-10-25 17:36:03 +0700

    add url for static file

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from restaurant.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    path('api-token-auth/', obtain_auth_token)
+    path('api-token-auth/', obtain_auth_token),
+    path('', index),
 ]
